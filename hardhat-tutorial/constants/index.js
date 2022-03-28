72de4bef28a5e2540a9c766e96ce1765586e84f1
PeterVermeulen01123 <94803849+PeterVermeulen01123@users.noreply.github.com>
2022-03-28 13:14:44 -0400

    adding initial files to repo

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xfc64041D31fBeAd3e97a3f0F5b9E447fc928CB7a";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
